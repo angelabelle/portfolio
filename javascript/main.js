4fb16d3847e179394fc3260b25be10c5b2887fb8
Angelabelle Abarientos <ab.abarientos@gmail.com>
2018-05-12 16:43:40 -0700

    more images

---
+++ javascript/main.js
@@ -0,0 +1,91 @@
+
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+function on2() {
+    document.getElementById("overlay-2").style.display = "block";
+}
+
+function off2() {
+    document.getElementById("overlay-2").style.display = "none";
+}
+
+
+function on3() {
+    document.getElementById("overlay-3").style.display = "block";
+}
+
+function off3() {
+    document.getElementById("overlay-3").style.display = "none";
+}
+
+function on4() {
+    document.getElementById("overlay-4").style.display = "block";
+}
+
+function off4() {
+    document.getElementById("overlay-4").style.display = "none";
+}
+
+
+function on5() {
+    document.getElementById("overlay-5").style.display = "block";
+
+}
+
+function off5() {
+    document.getElementById("overlay-5").style.display = "none";
+    var iframe = document.querySelector('iframe');
+        var player = new Vimeo.Player(iframe);
+
+        player.on('play', function() {
+            console.log('played the video!');
+        });
+
+        player.getVideoTitle().then(function(title) {
+            console.log('title:', title);
+        });
+
+
+    if ( iframe ) {
+   var player = new Vimeo.Player( iframe )
+   player.pause()
+
+ }
+
+}
+
+
+function on6() {
+    document.getElementById("overlay-6").style.display = "block";
+}
+
+function off6() {
+    document.getElementById("overlay-6").style.display = "none";
+}
+
+
+function on7() {
+    document.getElementById("overlay-7").style.display = "block";
+}
+
+function off7() {
+    document.getElementById("overlay-7").style.display = "none";
+}
+
+
+function on8() {
+    document.getElementById("overlay-8").style.display = "block";
+}
+
+function off8() {
+    document.getElementById("overlay-8").style.display = "none";
+}

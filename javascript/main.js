619b1c533b59f49019603c061ddbcb50369c8aba
Angelabelle Abarientos <34222276+angelabelle@users.noreply.github.com>
2019-11-28 00:04:26 -0800

    update for safari

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,22 @@ function myFunction() {
   var x = document.getElementById("navBar");
   if (x.style.display === "block") {
     x.style.display = "none";
-  } else {
-    x.style.display = "block";
   }
+   else  {
+     x.style.display = "block";
+   }
+}
+
+function displayNav()
+{
+    var x = document.getElementById("navBar");
+    var w = window.outerWidth;
+    console.log("hello");
+    if (w >= 601)
+    {
+         x.style.display = "block";
+    }
+    else {
+         x.style.display = "none";
+    }
 }
